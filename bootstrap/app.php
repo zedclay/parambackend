9057ea5a868d2725a8d89fd9ee8677b6a10a8ffb
zedclay <abdou31bousbaa@gmail.com>
2025-12-06 02:18:19 +0100

    hello

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,7 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        apiPrefix: '', // Remove /api prefix since .htaccess handles it
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
